a162a2cd6a67e2c9620b17493c63a453fd157e89
ysrs <lu_lu_liu@163.com>
2024-04-27 19:09:11 +0800

    steaming 模式测试

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
 	"github.com/tmc/langchaingo/prompts"
+	"log"
 	"net/http"
 )
 
 func main() {
+	testStream()
+
 	r := gin.Default()
 	v1 := r.Group("/api/v1")
 	{
@@ -17,6 +21,34 @@ func main() {
 		v1.POST("/translate", translate)
 	}
 	r.Run(":8080")
+}
+
+func testStream() {
+	llm, err := ollama.New(ollama.WithModel("qwen"))
+	if err != nil {
+		log.Fatal("error", err.Error())
+		return
+	}
+
+	content, err := llm.GenerateContent(context.Background(),
+		[]llms.MessageContent{
+			llms.TextParts(llms.ChatMessageTypeHuman, "天空为什么事蓝色的"),
+		},
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			fmt.Printf("%v", string(chunk))
+			return nil
+		}),
+	)
+	if err != nil {
+		log.Fatal("error", err.Error())
+		return
+	}
+
+	if content.Choices[0].FuncCall != nil {
+		fmt.Printf("function call: %v\n", content.Choices[0].FuncCall)
+	}
+
+	fmt.Printf("streaming test over\n")
 }
 
 func generateResponse(c *gin.Context) {
